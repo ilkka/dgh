7335cd623f5dfd45da41783f72d4af2d8aa92a0e
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-05-19 11:38:09 +0300

    Configure rspec and cuc for spork

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,13 +1,22 @@
-require 'bundler'
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  require 'bundler'
+  begin
+    Bundler.setup(:default, :development)
+  rescue Bundler::BundlerError => e
+    $stderr.puts e.message
+    $stderr.puts "Run `bundle install` to install missing gems"
+    exit e.status_code
+  end
+
+  require 'rspec/expectations'
+
+  $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
 end
 
-$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
-require 'dgh'
+Spork.each_run do
+  require 'dgh'
+end
 
-require 'rspec/expectations'
