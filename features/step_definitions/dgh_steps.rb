8a72b0e29360d560fda144d6417bb6915b392cff
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-05-19 11:45:33 +0300

    Pending steps

---
+++ features/step_definitions/dgh_steps.rb
@@ -0,0 +1,11 @@
+Given /^that there are no downgradable packages$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I run dgh$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^no packages should be reported as downgrade candidates$/ do
+  pending # express the regexp above with the code you wish you had
+end
