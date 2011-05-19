57d6f670da3c21aa5e88018efd3940fb28cf2f11
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-05-19 12:07:52 +0300

    Steps for first feature that just invoke the binary

--- features/step_definitions/dgh_steps.rb
+++ features/step_definitions/dgh_steps.rb
@@ -1,11 +1,11 @@
 Given /^that there are no downgradable packages$/ do
-  pending # express the regexp above with the code you wish you had
+  # pass
 end
 
 When /^I run dgh$/ do
-  pending # express the regexp above with the code you wish you had
+  @output = `#{File.join(File.dirname(__FILE__), '..', '..', 'bin', 'dgh')}`
 end
 
 Then /^no packages should be reported as downgrade candidates$/ do
-  pending # express the regexp above with the code you wish you had
+  @output.length.should == 0
 end
