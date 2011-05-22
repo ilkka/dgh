4d7338e7044296d5a0d4bccc592ae11bb2d4d5b9
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-05-22 20:01:00 +0300

    Properly escape backslash

--- lib/dgh.rb
+++ lib/dgh.rb
@@ -30,7 +30,7 @@ module Dgh
         puts "dgh, the Debian/Ubuntu Downgrade Helper"
         puts "Usage: dgh <apt-cache policy listing>"
         puts "You can generate a policy listing by running e.g."
-        puts "`dpkg --get-selections|egrep '\binstall'|awk '{print $1}'|\\"
+        puts "`dpkg --get-selections|egrep '\\binstall'|awk '{print $1}'|\\"
         puts "  xargs env LANG=C apt-cache policy`"
       else
         ARGV.each do |f|
