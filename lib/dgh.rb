a7d5ad0ca2811f9e7df11891f64a58668eb69d4c
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-05-22 17:20:50 +0300

    Implement finding downgradables

--- lib/dgh.rb
+++ lib/dgh.rb
@@ -4,9 +4,24 @@ require 'policy'
 module Dgh  
   extend self
 
+  # Parse input with the policy grammar
+  #
+  # @param input [String] apt-cache policy input
+  # @return a list of hashes with info on packages
   def parse(input)
     @parser ||= PolicyParser.new
     @parser.parse input
+  end
+
+  # Find downgradable packages
+  #
+  # @param paca
+  def find_downgradable(packages)
+    packages.select do |pkg|
+      pkg[:versions].any? do |ver|
+        ver[:current] && ver[:sources].length == 1 && ver[:sources][0][:source] == '/var/lib/dpkg/status'
+      end
+    end
   end
 
   def run
