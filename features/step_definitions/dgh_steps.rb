b3f64a4a18351c70cc31d0519feff1d17703a478
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-09-18 20:16:38 +0300

    Test both downgradables and no downgradables scenarios

--- features/step_definitions/dgh_steps.rb
+++ features/step_definitions/dgh_steps.rb
@@ -1,11 +1,20 @@
-Given /^that there are no downgradable packages$/ do
-  # pass
+Given /^there are no downgradable packages$/ do
+  @policyfile = File.join(File.dirname(__FILE__), '..', 'support', 'policy_no_candidates.out') 
 end
 
 When /^I run dgh$/ do
-  @output = `#{File.join(File.dirname(__FILE__), '..', '..', 'bin', 'dgh')} #{File.join(File.dirname(__FILE__), '..', 'support', 'policy.out')}`
+  @output = `#{File.join(File.dirname(__FILE__), '..', '..', 'bin', 'dgh')} #{@policyfile}`
 end
 
 Then /^no packages should be reported as downgrade candidates$/ do
   @output.should == ""
 end
+
+Given /^there are some downgradable packages$/ do
+  @policyfile = File.join(File.dirname(__FILE__), '..', 'support', 'policy.out') 
+end
+
+Then /^those packages should be reported as downgrade candidates$/ do
+  @output.should_match /git-flow\nveracity\npicasa/
+end
+
